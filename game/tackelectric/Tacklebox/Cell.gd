a19ca529a8f7242cf66f90a0390f14e59990515f
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-06-15 21:04:42 -0700

    Cell.gd: Changed cells to just handle references to entities, and not their destruction
    Entities now handle their current pos, and a reference to an entity, but since they are no longer the parent, they will no longer be the one that frees the entity

--- game/tackelectric/Tacklebox/Cell.gd
+++ game/tackelectric/Tacklebox/Cell.gd
@@ -2,15 +2,15 @@ extends Node3D
 class_name Cell
 
 @export var held_entity : Entity = null
-@export var held_compartment : Compartment = null
 
-func addEntity(entity):
-	add_child(entity)
+var gridPos := Vector2(0,0)
+
+func setEntity(entity):
 	held_entity = entity
 	print("Cell: Added entity")
 
 func removeEntity():
-	if is_instance_valid(held_entity):
-		held_entity.queue_free()
-	
 	held_entity = null
+
+func getEntity():
+	return held_entity
