10bce259647d4b771bf9c8c3b6df94c2d2f65601
suarez <danny@suarez.fm>
2025-06-18 21:52:43 -0700

    added options page and volume slider

--- game/tackelectric/UI/menus/menu_list.gd
+++ game/tackelectric/UI/menus/menu_list.gd
@@ -1,6 +1,7 @@
 extends VBoxContainer
 
 @export var tacklebox : PackedScene = null
+@export var options : PackedScene = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,4 +15,9 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	g.game.loadScene(tacklebox, $"../../../..")
+	g.game.unloadScene($".")
+
+
+func _on_options_pressed() -> void:
+	g.game.loadScene(options, $"../../../..")
 	g.game.unloadScene($".")
