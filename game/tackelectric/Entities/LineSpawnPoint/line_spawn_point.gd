730ff11026cf8eed417726fb10bd5cbd9d953863
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-06-20 12:06:02 -0700

    line_spawn_point.gd: Added the tool script extension to allow the entity class to change its position in the editor

--- game/tackelectric/Entities/LineSpawnPoint/line_spawn_point.gd
+++ game/tackelectric/Entities/LineSpawnPoint/line_spawn_point.gd
@@ -1,6 +1,8 @@
+@tool
 extends Entity
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("spawn_line"):
-		emit_signal("spawnLine", anchorCell.gridPos)
+	if not Engine.is_editor_hint():
+		if event.is_action_pressed("spawn_line"):
+			emit_signal("spawnLine", anchorCell.gridPos)
