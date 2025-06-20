cc61b2c51c32cf75492c1d5eccd2becd39355bd8
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-06-19 23:19:15 -0700

    Entity.gd: Updated entity to handle directional entities and fishing line interactions

--- game/tackelectric/Entity.gd
+++ game/tackelectric/Entity.gd
@@ -3,6 +3,11 @@ class_name Entity
 
 @export var StartingCoord := Vector2(0,0)
 @export var Size := Vector2(1,1)
+@export var startingDirection := Vector2(0, 1)
+
+# When a fishing line checks the cell, it will try to find this
+# method, then it will check its own script for a matching method
+@export var lineContactMethod := ""
 
 # This is the cell in the grid that houses the top
 # left quadrant of the entity, if the entity 
@@ -10,4 +15,10 @@ class_name Entity
 # otherwise it is just the cell that it is currently on
 var anchorCell : Cell = null
 
+
 signal moveEntity
+
+# Used for the fishing line spawn point
+# if this was a longer project then we should 
+# do this in a better way
+signal spawnLine
