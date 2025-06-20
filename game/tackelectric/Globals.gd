2337ad07afeb904ef1b3fec94e99a6f57677d602
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-06-20 16:02:53 -0700

    Globals.gd: Removed unused ready and process functions from globals

--- game/tackelectric/Globals.gd
+++ game/tackelectric/Globals.gd
@@ -3,12 +3,3 @@ extends Node
 var camera = null
 
 @export var gridSeparation := .52
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
