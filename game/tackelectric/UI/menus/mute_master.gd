bee545dc40aeecf5b51f2358473f8472d4fb3e8f
suarez <danny@suarez.fm>
2025-06-20 19:27:16 -0700

    Added volume sliders and mute buttons

---
+++ game/tackelectric/UI/menus/mute_master.gd
@@ -0,0 +1,22 @@
+extends Button
+
+@export var audio_bus : String = "Master"
+
+@onready var _bus := AudioServer.get_bus_index(audio_bus)
+
+var isMuted : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if isMuted:
+		set_text("Mute %s (Muted)" % audio_bus)
+	else:
+		set_text("Mute %s (Unmuted)" % audio_bus)
+
+func _on_pressed() -> void:
+	AudioServer.set_bus_mute(_bus, !isMuted)
+	isMuted = !isMuted
