c77ad2c5e4e61246a658acf9a997590c6f40c77c
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-06-22 00:27:44 -0700

    globals.gd: Added some functions

--- game/tackelectric/Globals.gd
+++ game/tackelectric/Globals.gd
@@ -3,6 +3,11 @@ extends Node
 var camera = null
 
 @export var gridSeparation := .52
+@export var lineHeight := .25
+
+var selectingCompartment := false
+
+
 
 # Global signals so they can be triggered from multiple entities
 signal game_is_won
@@ -12,10 +17,15 @@ signal game_is_over
 func game_won():
 	print("Game won!")
 	emit_signal("game_is_won")
+	resetVariables()
 	get_tree().reload_current_scene()
 
 
 func game_over():
 	print("Game over!")
+	resetVariables()
 	emit_signal("game_is_over")
 	get_tree().reload_current_scene()
+
+func resetVariables():
+	selectingCompartment = false
