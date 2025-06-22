ad492b0a25136e2155f2c58a4b438b8204eccce2
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-06-22 14:51:48 -0700

    Lures: Added a model and some rotation

--- game/tackelectric/Entities/Lure/lure.gd
+++ game/tackelectric/Entities/Lure/lure.gd
@@ -4,12 +4,8 @@ extends Entity
 
 # Lure
 
-#func _ready():
-	#if not Engine.is_editor_hint():
-		#await get_tree().create_timer(.1).timeout
-		## Match the direction to the starting direction
-		#var angle = (Vector2(startingDirection.y, startingDirection.x)).angle()
-#
-		#var newRotation = rad_to_deg(angle)
-		#
-		#rotation_degrees.y = newRotation
+func _ready():
+	if not Engine.is_editor_hint():
+		await get_tree().create_timer(randf_range(.1,.6)).timeout
+		if !get_parent().get_parent() is Compartment:
+			$sway_anim.play("sway")
