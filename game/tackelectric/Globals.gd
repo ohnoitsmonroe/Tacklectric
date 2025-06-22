65c3e2a740de02b976b6f5a1846d283cb2e2bd1f
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-06-21 21:29:06 -0700

    Globals.gd: Added winning and losing global functions

--- game/tackelectric/Globals.gd
+++ game/tackelectric/Globals.gd
@@ -3,3 +3,19 @@ extends Node
 var camera = null
 
 @export var gridSeparation := .52
+
+# Global signals so they can be triggered from multiple entities
+signal game_is_won
+signal game_is_over
+
+
+func game_won():
+	print("Game won!")
+	emit_signal("game_is_won")
+	get_tree().reload_current_scene()
+
+
+func game_over():
+	print("Game over!")
+	emit_signal("game_is_over")
+	get_tree().reload_current_scene()
