a74a40f00cb9363b5a08a7754b58f2173eebd0a1
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-06-23 01:10:03 -0700

    fish

--- game/tackelectric/Entities/fish.gd
+++ game/tackelectric/Entities/fish.gd
@@ -2,3 +2,19 @@
 extends Entity
 
 # Fish
+
+func _ready():
+	if not Engine.is_editor_hint():
+		g.winText.connect(bite)
+	
+	g.revealProps.connect(reveal)
+	g.shrinkProps.connect(hideProp)
+
+func bite():
+	$anim.play("bite")
+
+func hideProp():
+	$anim.play("shrink")
+
+func reveal():
+	$anim.play("reveal")
