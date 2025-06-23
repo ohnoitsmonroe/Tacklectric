0a2c5d545d9c16bc2bf94d2f2ecedd0ae66c80cf
Alexander Elder <33436918+Alfaa123@users.noreply.github.com>
2025-06-22 21:03:28 -0400

    Fixed audio muting
    Old 'isMuted' variable was being shared between all 3 muting button scripts. This change simply removes that variable and uses the AudioServer.is_bus_mute function directly instead.

--- game/tackelectric/UI/menus/mute_music.gd
+++ game/tackelectric/UI/menus/mute_music.gd
@@ -4,19 +4,16 @@ extends Button
 
 @onready var _bus := AudioServer.get_bus_index(audio_bus)
 
-var isMuted : bool = AudioServer.is_bus_mute(_bus)
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if isMuted:
+	if AudioServer.is_bus_mute(_bus):
 		set_text("Mute %s (Muted)" % audio_bus)
 	else:
 		set_text("Mute %s (Unmuted)" % audio_bus)
 
 func _on_pressed() -> void:
-	AudioServer.set_bus_mute(_bus, !isMuted)
-	isMuted = !isMuted
+	AudioServer.set_bus_mute(_bus, not AudioServer.is_bus_mute(_bus))
