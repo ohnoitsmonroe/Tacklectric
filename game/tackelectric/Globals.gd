ce0f89157f0f4d881a332975a13a0dfc7226657f
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-06-22 20:24:24 -0700

    globals: added the global level loaded signal

--- game/tackelectric/Globals.gd
+++ game/tackelectric/Globals.gd
@@ -17,7 +17,7 @@ var selectingCompartment := false
 # Global signals so they can be triggered from multiple entities
 signal game_is_won
 signal game_is_over
-
+signal level_loaded
 
 func game_won():
 	print("Game won!")
@@ -29,16 +29,16 @@ func game_won():
 
 	#get_tree().reload_current_scene()
 
+func load_level():
+	emit_signal("level_loaded")
 
 func game_over():
 	print("Game over!")
-
-
-	await get_tree().create_timer(1.5).timeout
 	resetVariables()
 	
 	emit_signal("game_is_over")
-	#get_tree().reload_current_scene()
+	
+
 
 func resetVariables():
 	selectingCompartment = false
