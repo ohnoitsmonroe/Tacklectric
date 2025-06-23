c90738a3c264e34186488c9a7263f2cdd19b3413
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-06-22 20:20:49 -0700

    tacklebox model: did some stuff here

--- game/tackelectric/Tacklebox/tacklebox_model.gd
+++ game/tackelectric/Tacklebox/tacklebox_model.gd
@@ -2,8 +2,20 @@ extends Node3D
 
 # Tacklebox model
 
+func _ready() -> void:
+	g.game_is_over.connect(closeAndReOpen)
+	g.level_loaded.connect(closeAndReOpen)
+
+
+func closeAndReOpen():
+	close()
+	await get_tree().create_timer(1).timeout
+	open()
+
+
 func open():
 	$anim.play("open")
+
 
 func close():
 	$anim.play("close")
