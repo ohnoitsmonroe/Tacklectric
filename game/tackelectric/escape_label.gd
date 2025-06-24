edec86ee52d1c94af3e74546683440f1ef4ad9dc
suarez <danny@suarez.fm>
2025-06-23 19:25:37 -0700

    fade in escape label

--- game/tackelectric/escape_label.gd
+++ game/tackelectric/escape_label.gd
@@ -6,9 +6,28 @@ var menu : PackedScene = preload("res://UI/menus/menu_list.tscn")
 var options : PackedScene = preload("res://UI/menus/options_list.tscn")
 var levels : PackedScene = preload("res://UI/menus/level_list.tscn")
 
+#var tween = null
+
+@onready var tween = get_tree().create_tween()
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	modulate.a = 0.0
+	tween.tween_property(self, "modulate:a", 1.0, 3.0)
+	tween.stop()
+
+	match g.currentScene:
+		menu:
+			set_text("[right]keep holding escape to go to quit[/right]")
+		options:
+			set_text("[right]keep holding escape to go back to the main menu[/right]")
+		levels:
+			set_text("[right]keep holding escape to go back to the main menu[/right]")
+		_:
+			set_text("[right]keep holding escape to go back to the level list[/right]")
+			
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,11 +36,14 @@ func _process(delta: float) -> void:
 
 func _input(event):
 	if event.is_action_pressed("escape"):
+		#visible = true
+		tween.play()
 		timer.start()
-		visible = true
 	if event.is_action_released("escape"):
+		tween.stop()
+		modulate.a = 0.0
 		timer.stop()
-		visible = false
+		#visible = false
 
 
 func _on_escape_timer_timeout() -> void:
@@ -35,4 +57,5 @@ func _on_escape_timer_timeout() -> void:
 		_:
 			g.game.loadLevelList()
 	
-	visible = false
+	#visible = false
+	modulate.a = 0.0
