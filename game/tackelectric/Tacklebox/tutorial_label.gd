e190db0000f1d3918c563fac19e4acc8fea89e9f
ohnoitsmonroe <milton.p.monroe@gmail.com>
2025-06-23 22:35:08 -0700

    changed text color

--- game/tackelectric/Tacklebox/tutorial_label.gd
+++ game/tackelectric/Tacklebox/tutorial_label.gd
@@ -7,15 +7,15 @@ var didMoveTut : bool = false
 var didCastTut : bool = false
 var didRetryTut : bool = false
 
-var moveTutorial : String = "[center][color=#000000]Click and drag the lures on the board with a wood plank underneath.
+var moveTutorial : String = "[center]Click and drag the lures on the board with a wood plank underneath.
 Lures without a plank cannot be moved.
 You can move multiple tiles in one turn, but only in one direction.[/color][/center]"
 
-var castTutorial : String = "[center][color=#000000]Press spacebar to cast the fishing line from the top left.
+var castTutorial : String = "[center]Press spacebar to cast the fishing line from the top left.
 It will travel the path shown by the lures. Your goal is to reach the fish on the right.
 You have as many turns as there red lights on the left side of the tacklebox.[/color][/center]"
 
-var retryTutorial : String = "[center][color=#000000]Press R to restart the level at any time.
+var retryTutorial : String = "[center]Press R to restart the level at any time.
 Hold ESC at any time to go back to the previous menu, or quit the game if in the main menu.[/color][/center]"
 
 # Called when the node enters the scene tree for the first time.
@@ -42,7 +42,7 @@ func _ready() -> void:
 		tween.tween_property(self, "modulate:a", 1.0, 5.0).set_delay(2.5)
 	else:
 		modulate.a = 1.0
-		set_text("[center][color=#000000]Now try to reach the fish! Good luck, and thank you for playing our game.[/color][/center]")
+		set_text("[center]Now try to reach the fish! Good luck, and thank you for playing our game.[/color][/center]")
 		tween.tween_property(self, "modulate:a", 0.0, 5.0).set_delay(2.5)
 		tween.play()
 		g.sawLastTutorial = true
