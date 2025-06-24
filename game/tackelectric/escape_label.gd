696c4523577044ce4595d1dd8663c8bba3c5210c
suarez <danny@suarez.fm>
2025-06-23 20:39:13 -0700

    tutorial text implemented

--- game/tackelectric/escape_label.gd
+++ game/tackelectric/escape_label.gd
@@ -2,9 +2,9 @@ extends RichTextLabel
 
 @onready var timer = $EscapeTimer
 
-var menu : PackedScene = preload("res://UI/menus/menu_list.tscn")
-var options : PackedScene = preload("res://UI/menus/options_list.tscn")
-var levels : PackedScene = preload("res://UI/menus/level_list.tscn")
+@onready var menu : PackedScene = preload("res://UI/menus/menu_list.tscn")
+@onready var options : PackedScene = preload("res://UI/menus/options_list.tscn")
+@onready var levels : PackedScene = preload("res://UI/menus/level_list.tscn")
 
 #var tween = null
 
@@ -14,19 +14,10 @@ var levels : PackedScene = preload("res://UI/menus/level_list.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	visible = false
 	modulate.a = 0.0
 	tween.tween_property(self, "modulate:a", 1.0, 3.0)
 	tween.stop()
-
-	match g.currentScene:
-		menu:
-			set_text("[right]keep holding escape to go to quit[/right]")
-		options:
-			set_text("[right]keep holding escape to go back to the main menu[/right]")
-		levels:
-			set_text("[right]keep holding escape to go back to the main menu[/right]")
-		_:
-			set_text("[right]keep holding escape to go back to the level list[/right]")
 			
 
 
@@ -35,15 +26,24 @@ func _process(delta: float) -> void:
 	pass
 
 func _input(event):
+	match g.currentScene:
+		menu:
+			set_text("[center]keep holding escape to go to quit[/center]")
+		options:
+			set_text("[center]keep holding escape to go back to the main menu[/center]")
+		levels:
+			set_text("[center]keep holding escape to go back to the main menu[/center]")
+		_:
+			set_text("[center]keep holding escape to go back to the level list[/center]")
 	if event.is_action_pressed("escape"):
-		#visible = true
+		visible = true
 		tween.play()
 		timer.start()
 	if event.is_action_released("escape"):
 		tween.stop()
 		modulate.a = 0.0
 		timer.stop()
-		#visible = false
+		visible = false
 
 
 func _on_escape_timer_timeout() -> void:
@@ -57,5 +57,6 @@ func _on_escape_timer_timeout() -> void:
 		_:
 			g.game.loadLevelList()
 	
-	#visible = false
+	visible = false
 	modulate.a = 0.0
+	tween.stop()
